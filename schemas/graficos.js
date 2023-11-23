cfeaecdc2ba65dd6a4560e49d64df95392ddd31e
Crautor <vitor.dallanol@gmail.com>
2023-11-23 13:16:43 -0300

    att legenda

--- schemas/graficos.js
+++ schemas/graficos.js
@@ -8,16 +8,37 @@ export default {
         type: 'image',
         title: 'Grafico Satisfação Biopark'
       },
+
+      {
+        name: 'legendasatisfacaobpk',
+        type: 'string',
+        title: 'Legenda Satisfação Biopark'
+      },
+
       {
         name: 'graficosatisfacaocurso',
         type: 'image',
         title: 'Grafico Satisfação Cursos'
       },
+
+      {
+        name: 'legendasatisfacaocurso',
+        type: 'string',
+        title: 'Legenda Satisfação Cursos'
+      },
+
       {
         name: 'graficosatisfacaoinfra',
         type: 'image',
         title: 'Grafico Satisfação Infraestrutura' 
       },
+
+      {
+        name: 'legendasatisfacaoinfra',
+        type: 'string',
+        title: 'Legenda Satisfação Infraestrutura'
+      },
+
     ]
   }
 
